b04e144d2fea37580b81e2fb1fafaab16cc605a0
Pedro Cuenca <pedro@huggingface.co>
2023-07-17 12:36:06 +0200

    BPE tokenizer tests.
    Llama fails because we don't find normalizers or pre-processors.

---
+++ Tests/TokenizersTests/BPETokenizerTests.swift
@@ -0,0 +1,86 @@
+//
+//  BPETokenizerTests.swift
+//
+//  Created by Pedro Cuenca on July 2023.
+//  Based on GPT2TokenizerTests by Julien Chaumond.
+//  Copyright © 2023 Hugging Face. All rights reserved.
+//
+
+import XCTest
+@testable import Hub
+@testable import Tokenizers
+
+struct BPEEncodingSampleDataset: Decodable {
+    let text: String
+    let bpe_tokens: [String]
+    let token_ids: [Int]
+}
+
+class BPETokenizerTests {
+    // Resources in test bundle.
+    // TODO: download from the Hub.
+    let tokenizerConfigFilename: String
+    let tokenizerDataFilename: String
+    let encodedSamplesFilename: String
+    
+    init(tokenizerConfigFilename: String, tokenizerDataFilename: String, encodedSamplesFilename: String) {
+        self.tokenizerConfigFilename = tokenizerConfigFilename
+        self.tokenizerDataFilename = tokenizerDataFilename
+        self.encodedSamplesFilename = encodedSamplesFilename
+    }
+    
+    lazy var dataset: BPEEncodingSampleDataset = {
+        let url = Bundle.module.url(forResource: encodedSamplesFilename, withExtension: "json")!
+        let json = try! Data(contentsOf: url)
+        let decoder = JSONDecoder()
+        let dataset = try! decoder.decode(BPEEncodingSampleDataset.self, from: json)
+        return dataset
+    }()
+    
+    lazy var tokenizer: GPT2Tokenizer = {
+        let tokenizerConfig = {
+            let url = Bundle.module.url(forResource: tokenizerConfigFilename, withExtension: "json")!
+            let data = try! Data(contentsOf: url)
+            let parsed = try! JSONSerialization.jsonObject(with: data, options: [])
+            let dictionary = parsed as! [String: Any]
+            return Config(dictionary)
+        }()
+        
+        let tokenizerData = {
+            let url = Bundle.module.url(forResource: tokenizerDataFilename, withExtension: "json")!
+            let data = try! Data(contentsOf: url)
+            let parsed = try! JSONSerialization.jsonObject(with: data, options: [])
+            let dictionary = parsed as! [String: Any]
+            return Config(dictionary)
+        }()
+        
+        guard let vocab = tokenizerData.model?.vocab?.dictionary as? [String: Int] else { fatalError("Cannot find vocab in tokenizer JSON") }
+        let merges = tokenizerData.model?.merges?.value as? [String]
+
+        return GPT2Tokenizer(vocab: vocab, merges: merges)
+    }()
+        
+    func testTokenize() {        
+        XCTAssertEqual(
+            tokenizer.tokenize(text: dataset.text),
+            dataset.bpe_tokens
+        )
+    }
+    
+    func testEncode() {
+        XCTAssertEqual(
+            tokenizer.encode(text: dataset.text),
+            dataset.token_ids
+        )
+    }
+    
+    func testDecode() {
+        print(
+            tokenizer.decode(tokens: dataset.token_ids)
+        )
+        XCTAssertEqual(
+            tokenizer.decode(tokens: dataset.token_ids),
+            dataset.text
+        )
+    }
+}
