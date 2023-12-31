dc67b5575904b773d46a5cc012c1a747330d8029
Pedro Cuenca <pedro@huggingface.co>
2023-12-31 12:31:16 +0100

    Improve handling of background download tasks

--- Sources/Hub/Downloader.swift
+++ Sources/Hub/Downloader.swift
@@ -33,18 +33,31 @@ class Downloader: NSObject, ObservableObject {
         self.destination = destination
         super.init()
         
-        // .background allows downloads to proceed in the background
         let config = URLSessionConfiguration.background(withIdentifier: url.path)
         urlSession = URLSession(configuration: config, delegate: self, delegateQueue: OperationQueue())
         downloadState.value = .downloading(0)
         urlSession?.getAllTasks { tasks in
             // If there's an existing pending background task with the same URL, let it proceed.
-            guard tasks.filter({ $0.originalRequest?.url == url }).isEmpty else {
-                print("Already downloading \(url)")
-                return
+            if let existing = tasks.filter({ $0.originalRequest?.url == url }).first {
+                switch existing.state {
+                case .running:
+                    // print("Already downloading \(url)")
+                    return
+                case .suspended:
+                    // print("Resuming suspended download task for \(url)")
+                    existing.resume()
+                    return
+                case .canceling:
+                    // print("Starting new download task for \(url), previous was canceling")
+                    break
+                case .completed:
+                    // print("Starting new download task for \(url), previous is complete but the file is no longer present (I think it's cached)")
+                    break
+                @unknown default:
+                    // print("Unknown state for running task; cancelling and creating a new one")
+                    existing.cancel()
+                }
             }
-            print("Starting download of \(url)")
-            
             var request = URLRequest(url: url)
             if let authToken = authToken {
                 request.setValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
@@ -100,8 +113,8 @@ extension Downloader: URLSessionDelegate, URLSessionDownloadDelegate {
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         if let error = error {
             downloadState.value = .failed(error)
-        } else if let response = task.response as? HTTPURLResponse {
-            print("HTTP response status code: \(response.statusCode)")
+//        } else if let response = task.response as? HTTPURLResponse {
+//            print("HTTP response status code: \(response.statusCode)")
 //            let headers = response.allHeaderFields
 //            print("HTTP response headers: \(headers)")
         }
