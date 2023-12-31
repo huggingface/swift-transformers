c70b45bce302cd4787492dacc6cceb7e66d9347d
Pedro Cuenca <pedro@huggingface.co>
2023-12-31 12:37:49 +0100

    Add comment

--- Sources/Hub/Downloader.swift
+++ Sources/Hub/Downloader.swift
@@ -1,7 +1,7 @@
 //
 //  Downloader.swift
-//  Diffusion
 //
+//  Adapted from https://github.com/huggingface/swift-coreml-diffusers/blob/d041577b9f5e201baa3465bc60bc5d0a1cf7ed7f/Diffusion/Common/Downloader.swift
 //  Created by Pedro Cuenca on December 2022.
 //  See LICENSE at https://github.com/huggingface/swift-coreml-diffusers/LICENSE
 //
