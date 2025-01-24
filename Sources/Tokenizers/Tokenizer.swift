1fab24c79cc8cb1da985d11c0eca1ecf4aaccf0e
Pedro Cuenca <pedro@huggingface.co>
2025-01-24 20:11:25 +0100

    Deepseek R1 tokenization support and fixes (#159)
    * Update Jinja, add Qwen R1 test
    
    * More robust added token support
    
    Serialized AddedToken class partially supported (in addition to String
    values)
    
    * Actually pass added tokens to decoder
    
    * Temporarily revert jinja upgrade

--- Sources/Tokenizers/Tokenizer.swift
+++ Sources/Tokenizers/Tokenizer.swift
@@ -41,6 +41,17 @@ public protocol TokenizingModel {
     var fuseUnknownTokens: Bool { get }
 }
 
+// Helper - possibly to be moved somewhere else
+func addedTokenAsString(_ addedToken: Config?) -> String? {
+    guard let addedToken = addedToken else { return nil }
+    if let stringValue = addedToken.stringValue {
+        return stringValue
+    }
+    // This is possibly a serialization of the AddedToken class
+    // TODO: support lstrip, rstrip, normalized, etc.
+    return addedToken.content?.stringValue
+}
+
 public extension TokenizingModel {
     func callAsFunction(_ text: String) -> [String] {
         tokenize(text: text)
@@ -241,7 +252,7 @@ public class PreTrainedTokenizer: Tokenizer {
         self.preTokenizer = PreTokenizerFactory.fromConfig(config: tokenizerData.preTokenizer)
         self.normalizer = NormalizerFactory.fromConfig(config: tokenizerData.normalizer)
         self.postProcessor = PostProcessorFactory.fromConfig(config: tokenizerData.postProcessor)
-        self.decoder = DecoderFactory.fromConfig(config: tokenizerData.decoder)
+        self.decoder = DecoderFactory.fromConfig(config: tokenizerData.decoder, addedTokens: self.addedTokens)
         self.cleanUpTokenizationSpaces = tokenizerConfig.cleanUpTokenizationSpaces?.boolValue ?? true
         self.tokenizerConfig = tokenizerConfig
 
@@ -421,7 +432,13 @@ public class PreTrainedTokenizer: Tokenizer {
         // TODO: maybe keep NSString here
         for (key, value) in tokenizerConfig.dictionary as [String : Any] {
             if specialTokenAttributes.contains(key), !(value is NSNull) {
-                context[key] = value
+                if let stringValue = value as? String {
+                    context[key] = stringValue
+                } else if let dictionary = value as? [NSString:Any] {
+                    context[key] = addedTokenAsString(Config(dictionary))
+                } else {
+                    context[key] = value
+                }
             }
         }
 
