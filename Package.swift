508412a7d2565b4b8de4ec5a832d0974ed81639e
Anthony DePasquale <anthony@depasquale.org>
2025-01-26 13:13:24 +0100

    Patch Jinja

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
-        .package(url: "https://github.com/johnmai-dev/Jinja", from: "1.1.0")
+//        .package(url: "https://github.com/johnmai-dev/Jinja", from: "1.1.0"),
+        .package(url: "https://github.com/DePasqualeOrg/Jinja", branch: "fix-objectvalue"),
     ],
     targets: [
         .executableTarget(
