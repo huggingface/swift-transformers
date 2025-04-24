9bf2c035973ab39dae445ee49560d55d39d551f4
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2025-04-24 11:10:20 +0200

    Package.swift reverted

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-transformers",
-    platforms: [.iOS(.v17), .macOS(.v14)],
+    platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         .library(name: "Transformers", targets: ["Tokenizers", "Generation", "Models"]),
         .executable(name: "transformers", targets: ["TransformersCLI"]),
