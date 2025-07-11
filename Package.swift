50c73c41c535ee02add412a82edd26abd5933ded
Pedro Cuenca <pedro@huggingface.co>
2025-07-11 16:31:34 +0200

    Upgrade to jinja 1.2.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.4.0")),
-        .package(url: "https://github.com/johnmai-dev/Jinja", .upToNextMinor(from: "1.2.0")),
+        .package(url: "https://github.com/johnmai-dev/Jinja", .upToNextMinor(from: "1.2.1")),
     ],
     targets: [
         .executableTarget(
