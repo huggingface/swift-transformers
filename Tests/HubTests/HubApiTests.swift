43bc90136a812c25a63cddf8ed90506fec50ee8d
Mattt Zmuda <mattt@me.com>
2025-09-11 05:37:39 -0700

    Remove empty overrides for setup and teardown

--- Tests/HubTests/HubApiTests.swift
+++ Tests/HubTests/HubApiTests.swift
@@ -9,14 +9,6 @@ import XCTest
 @testable import Hub
 
 class HubApiTests: XCTestCase {
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     // TODO: use a specific revision for these tests
 
     func testFilenameRetrieval() async {
