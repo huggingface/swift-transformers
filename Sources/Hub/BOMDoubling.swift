7f37a944a429baf5d7e194b19e009368565a0994
Pedro Cuenca <pedro@huggingface.co>
2025-09-12 16:51:15 +0200

    Workaround for tokens that start with Unicode BOM
    The BOM is swallowed by JSONSerialization.jsonObject() because it thinks
    it signals the encoding in use. This workaround duplicates BOM sequences
    that follow a quote character.
    
    Fixes #116
    Fixes #88
    Fixes https://github.com/ml-explore/mlx-swift-examples/issues/50

---
+++ Sources/Hub/BOMDoubling.swift
@@ -0,0 +1,48 @@
+//
+//  BOMDoubling.swift
+//  swift-transformers
+//
+//  Created by Pedro Cuenca on 20250912
+//
+
+import Foundation
+
+extension Data {
+    /// Workaround for https://github.com/huggingface/swift-transformers/issues/116
+    /// Duplicate a BOM sequence that follows a quote. The first BOM is swallowed by JSONSerialization.jsonObject
+    /// because it thinks it marks the encoding.
+    var duplicatingBOMsAfterQuotes: Data {
+        return self.withUnsafeBytes { (raw: UnsafeRawBufferPointer) in
+            let src = raw.bindMemory(to: UInt8.self)
+            var out = [UInt8]()
+            // We expect very few matches (only 6 for Gemma)
+            out.reserveCapacity(src.count + 1000)
+
+            var i = 0
+            while i < src.count {
+                let b = src[i]
+                out.append(b)
+
+                // Check for \u{feff} BOM (observed in Gemma tokenizers), which is encoded as 0xef 0xbb 0xbf.
+                // We may need more combinations.
+                if b == 0x22, i + 3 < src.count,
+                   src[i+1] == 0xEF, src[i+2] == 0xBB, src[i+3] == 0xBF {
+                    // Duplicate BOM
+                    out.append(0xEF); out.append(0xBB); out.append(0xBF)
+                    out.append(0xEF); out.append(0xBB); out.append(0xBF)
+                    i += 4
+                } else {
+                    i += 1
+                }
+            }
+            return Data(out)
+        }
+    }
+
+}
+
+extension JSONSerialization {
+    class func bomPreservingJsonObject(with data: Data, options: JSONSerialization.ReadingOptions = []) throws -> Any {
+        try JSONSerialization.jsonObject(with: data.duplicatingBOMsAfterQuotes, options: options)
+    }
+}
