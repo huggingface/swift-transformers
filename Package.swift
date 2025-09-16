fbe4de03253e169fcad7b5a782aaf4e3878a3096
Pedro Cuenca <pedro@huggingface.co>
2025-09-16 17:38:52 +0200

    Some commit.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
