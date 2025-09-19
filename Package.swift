a814cc530be5d1f847b532b1a53e54147a149e9a
Mattt <mattt@me.com>
2025-09-19 03:21:22 -0700

    Make consistent module resource locations (#246)
    * Rename FallbackConfigs directory to Resources
    
    * Move bert-vocab.txt to Resources directory
    
    * Add comment about fallback configuration file locations

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
         ),
         .executableTarget(name: "HubCLI", dependencies: ["Hub", .product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .target(name: "Generation", dependencies: ["Tokenizers"]),
-        .target(name: "Hub", resources: [.process("FallbackConfigs")], swiftSettings: swiftSettings),
+        .target(name: "Hub", resources: [.process("Resources")], swiftSettings: swiftSettings),
         .target(name: "Models", dependencies: ["Tokenizers", "Generation"]),
         .target(name: "Tokenizers", dependencies: ["Hub", .product(name: "Jinja", package: "Jinja")]),
         .testTarget(name: "GenerationTests", dependencies: ["Generation"]),
