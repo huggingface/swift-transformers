5a2e074bb1c198a173a541c3070985161f9e8537
Pedro Cuenca <pedro@huggingface.co>
2025-09-25 13:57:07 +0200

    Skip custom endpoint test

--- Tests/HubTests/HubApiTests.swift
+++ Tests/HubTests/HubApiTests.swift
@@ -274,6 +274,7 @@ class SnapshotDownloadTests: XCTestCase {
     }
 
     func testCustomEndpointDownload() async throws {
+        try XCTSkipIf(true, "Skipping, getting 429 from mirror")
         let hubApi = HubApi(downloadBase: downloadDestination, endpoint: "https://hf-mirror.com")
         var lastProgress: Progress? = nil
         let downloadedTo = try await hubApi.snapshot(from: repo, matching: "*.json") { progress in
