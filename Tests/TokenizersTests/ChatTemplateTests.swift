7a2fa758efeed0edbd8a57df8866be02124ce704
Mattt Zmuda <mattt@me.com>
2025-09-25 04:57:47 -0700

    Annotate @MainActor for tests using shared instance of tokenizer

--- Tests/TokenizersTests/ChatTemplateTests.swift
+++ Tests/TokenizersTests/ChatTemplateTests.swift
@@ -19,6 +19,7 @@ struct ChatTemplateTests {
         ]
     ]
 
+    @MainActor
     static let phiTokenizerTask = Task {
         try await AutoTokenizer.from(pretrained: "microsoft/Phi-3-mini-128k-instruct")
     }
@@ -27,6 +28,7 @@ struct ChatTemplateTests {
         try await phiTokenizerTask.value
     }
 
+    @MainActor
     static let tokenizerWithTemplateArrayTask = Task {
         try await AutoTokenizer.from(pretrained: "mlx-community/Mistral-7B-Instruct-v0.3-4bit")
     }
