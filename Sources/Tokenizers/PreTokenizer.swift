1d8e04c5a92ca39ee164df977cf28015156e0ec0
Mattt Zmuda <mattt@me.com>
2026-02-12 04:03:41 -0800

    Add documentation comment for punctuationRegex

--- Sources/Tokenizers/PreTokenizer.swift
+++ Sources/Tokenizers/PreTokenizer.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Hub
 
+/// Character class used by punctuation-based pre-tokenizers.
 private let punctuationRegex = #"\p{P}\u0021-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E"#
 
 /// Options that can be passed to pre-tokenization operations.
