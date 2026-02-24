33d6eec69293b79f248d1c5a53175f1bbf8c546b
Mattt Zmuda <mattt@me.com>
2026-02-24 05:04:27 -0800

    Bump swift-huggingface dependency to be from 0.8.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/huggingface/swift-jinja.git", from: "2.0.0"),
-        .package(url: "https://github.com/huggingface/swift-huggingface.git", from: "0.7.0"),
+        .package(url: "https://github.com/huggingface/swift-huggingface.git", from: "0.8.0"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", "3.0.0"..<"5.0.0"),
         .package(url: "https://github.com/ibireme/yyjson.git", exact: "0.12.0"),
